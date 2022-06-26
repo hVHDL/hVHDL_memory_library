bb9fb591e787fdfaddb83f4e86a12714e85a7b66
jari <jaarhonka@yahoo.com>
2022-06-26 19:28:41 +0300

    update ghdl

--- ghdl_compile_memory_library.bat
+++ ghdl_compile_memory_library.bat
@@ -4,9 +4,9 @@ echo %project_root%
 FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
 SET project_root=%%F
 )
-SET source=%project_root%/..
+SET source=%project_root%/
 
-ghdl -a --ieee=synopsys --std=08 %source%/memory_library/fpga_ram/ram_configuration/ram_configuration_16x1024_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/memory_library/fpga_ram/ram_read_port_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/memory_library/fpga_ram/ram_write_port_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/memory_library/fpga_ram/fpga_dual_port_ram_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/fpga_ram/ram_configuration/ram_configuration_16x1024_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/fpga_ram/ram_read_port_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/fpga_ram/ram_write_port_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/fpga_ram/fpga_dual_port_ram_pkg.vhd
