631ce2eba494975fd4bb3ed3acaaeb5e0ebba815
jari <jaarhonka@yahoo.com>
2022-06-26 12:37:27 +0300

    update memory library path in ghdl_compile

--- ghdl_compile_memory_library.bat
+++ ghdl_compile_memory_library.bat
@@ -4,7 +4,7 @@ echo %project_root%
 FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
 SET project_root=%%F
 )
-SET source=%project_root%/../memory_library
+SET source=%project_root%/..
 
-ghdl -a --ieee=synopsys --std=08 %source%/fpga_ram/ram_read_port_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/fpga_ram/fpga_dual_port_ram_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_memory_library/fpga_ram/ram_read_port_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %source%/hVHDL_memory_library//fpga_ram/fpga_dual_port_ram_pkg.vhd
