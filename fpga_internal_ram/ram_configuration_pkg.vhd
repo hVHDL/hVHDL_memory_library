29eaec0a8680c91a8666c8e4676c12f01a228571
Jari Honkanen <jaarhonka@yahoo.com>
2023-12-27 10:15:51 +0200

    update ram configuration

--- fpga_internal_ram/ram_configuration_pkg.vhd
+++ fpga_internal_ram/ram_configuration_pkg.vhd
@@ -6,7 +6,7 @@ package ram_configuration_pkg is
 
     -- make visible when using ram_read
     constant ram_bit_width : natural := 20;
-    constant ram_depth     : natural := 2**7;
+    constant ram_depth     : natural := 2**9;
 
     subtype address_integer is natural range 0 to ram_depth-1;
     subtype t_ram_data      is std_logic_vector(ram_bit_width-1 downto 0);
