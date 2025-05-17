42c96a239f7506185ab3db5886690225ab355021
Jari Honkanen <jaarhonka@yahoo.com>
2025-05-17 10:19:33 +0300

    fix rtl architecture of dp ram

--- testbench/dual_port_ram/arch_rtl_dp_ram_w_configurable_records.vhd
+++ testbench/dual_port_ram/arch_rtl_dp_ram_w_configurable_records.vhd
@@ -1,5 +1,5 @@
 
-architecture rtl of generic_dual_port_ram is
+architecture rtl of dual_port_ram is
 
 ------------------------------------------------------------------------
     ------------------------------
@@ -10,7 +10,7 @@ architecture rtl of generic_dual_port_ram is
     signal read_b_pipeline : std_logic_vector(1 downto 0) := (others => '0');
     signal output_b_buffer : std_logic_vector(ram_b_out.data'range);
 
-    constant ram_init : ram_array := initial_values;
+    constant ram_init : ram_array := g_ram_init_values;
     shared variable ram_contents : ram_array := ram_init;
 
 begin
@@ -23,9 +23,9 @@ begin
             read_a_pipeline <= read_a_pipeline(read_a_pipeline'left-1 downto 0) & ram_a_in.read_is_requested;
             ram_a_out.data  <= output_a_buffer;
             if (ram_a_in.read_is_requested = '1') or (ram_a_in.write_requested = '1') then
-                output_a_buffer <= ram_contents(ram_a_in.address);
+                output_a_buffer <= ram_contents(to_integer(ram_a_in.address));
                 if ram_a_in.write_requested = '1' then
-                    ram_contents(ram_a_in.address) := ram_a_in.data;
+                    ram_contents(to_integer(ram_a_in.address)) := ram_a_in.data;
                 end if;
             end if;
         end if;
@@ -37,9 +37,9 @@ begin
             read_b_pipeline <= read_b_pipeline(read_b_pipeline'left-1 downto 0) & ram_b_in.read_is_requested;
             ram_b_out.data  <= output_b_buffer;
             if (ram_b_in.read_is_requested = '1') or (ram_b_in.write_requested = '1') then
-                output_b_buffer <= ram_contents(ram_b_in.address);
+                output_b_buffer <= ram_contents(to_integer(ram_b_in.address));
                 if ram_b_in.write_requested = '1' then
-                    ram_contents(ram_b_in.address) := ram_b_in.data;
+                    ram_contents(to_integer(ram_b_in.address)) := ram_b_in.data;
                 end if;
             end if;
         end if;
